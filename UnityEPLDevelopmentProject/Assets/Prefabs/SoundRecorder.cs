65acb98b69b9b3a335dcdf5296960742fd472a5f
Henry Solberg <henry.j.solberg@gmail.com>
2017-08-11 17:10:36 -0400

    sound recording and video playing prefabs

---
+++ UnityEPLDevelopmentProject/Assets/Prefabs/SoundRecorder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundRecorder : MonoBehaviour
+{
+	public string outputPath = System.IO.Path.GetFullPath(".");
+
+	public void RecordClip(int secondsDuration)
+	{
+		AudioClip recording = Microphone.Start ("", false, secondsDuration, 44100);
+		string filePath = System.IO.Path.Combine(outputPath, "Recording" + System.DateTime.Now.Ticks);
+		SavWav.Save (filePath, recording);
+	}
+}
