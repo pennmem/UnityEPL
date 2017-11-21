9400167674cb9736a1c65b5e98559b0e85258419
Henry Jonas Solberg <milkshakeiii@gmail.com>
2017-11-21 15:10:32 -0500

    Jumping Cube Start Canvas

---
+++ UnityEPLDevelopmentProject/Assets/JumpingCubeExample/BeginExperiment.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeginExperiment : MonoBehaviour 
+{
+    public UnityEngine.UI.InputField participantNameInput;
+
+    public void Begin()
+    {
+        UnityEPL.SetExperimentName("Jumping Cube Experiment");
+        UnityEPL.AddParticipant(participantNameInput.text);
+    }
+}
