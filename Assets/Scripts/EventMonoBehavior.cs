3cddcd5fae0be327c6f928153eb4f79eab5ae08f
James Bruska <jamesbthecoder@gmail.com>
2023-02-07 16:42:19 -0500

    Added 2, 3, and 4 argument versions of Do functions in EventMonoBehavior

--- Assets/Scripts/EventMonoBehavior.cs
+++ Assets/Scripts/EventMonoBehavior.cs
@@ -16,8 +16,7 @@ public abstract class EventMonoBehaviour : MonoBehaviour {
         StartOverride();
     }
 
-    // TODO: JPB: Add support for cancellation token
-    // TODO: JPB: Add support for up to 4 arguments
+    // TODO: JPB: Add support for cancellation tokens in EventMonoBehavior Do functions
 
     // Do
 
@@ -27,11 +26,31 @@ public abstract class EventMonoBehaviour : MonoBehaviour {
     protected void Do(Func<IEnumerator> func) {
         DoHelper(func());
     }
-
     protected void Do<T>(Func<T, IEnumerator> func, T t)
             where T : struct {
         AssertBlittable(t);
         DoHelper(func(t));
+    }
+    protected void Do<T, U>(Func<T, U, IEnumerator> func, T t, U u)
+            where T : struct
+            where U : struct {
+        AssertBlittable(t, u);
+        DoHelper(func(t, u));
+    }
+    protected void Do<T, U, V>(Func<T, U, V, IEnumerator> func, T t, U u, V v)
+            where T : struct
+            where U : struct
+            where V : struct {
+        AssertBlittable(t, u, v);
+        DoHelper(func(t, u, v));
+    }
+    protected void Do<T, U, V, W>(Func<T, U, V, W, IEnumerator> func, T t, U u, V v, W w)
+            where T : struct
+            where U : struct
+            where V : struct
+            where W : struct {
+        AssertBlittable(t, u, v, w);
+        DoHelper(func(t, u, v, w));
     }
 
     // DoIn
@@ -40,11 +59,31 @@ public abstract class EventMonoBehaviour : MonoBehaviour {
         await InterfaceManager2.Delay(millisecondsDelay);
         Do(func);
     }
-
     protected async void DoIn<T>(int millisecondsDelay, Func<T, IEnumerator> func, T t)
             where T : struct {
         await InterfaceManager2.Delay(millisecondsDelay);
         Do(func, t);
+    }
+    protected async void DoIn<T, U>(int millisecondsDelay, Func<T, U, IEnumerator> func, T t, U u)
+            where T : struct
+            where U : struct {
+        await InterfaceManager2.Delay(millisecondsDelay);
+        Do(func, t, u);
+    }
+    protected async void DoIn<T, U, V>(int millisecondsDelay, Func<T, U, V, IEnumerator> func, T t, U u, V v)
+            where T : struct
+            where U : struct
+            where V : struct {
+        await InterfaceManager2.Delay(millisecondsDelay);
+        Do(func, t, u, v);
+    }
+    protected async void DoIn<T, U, V, W>(int millisecondsDelay, Func<T, U, V, W, IEnumerator> func, T t, U u, V v, W w)
+            where T : struct
+            where U : struct
+            where V : struct
+            where W : struct {
+        await InterfaceManager2.Delay(millisecondsDelay);
+        Do(func, t, u, v, w);
     }
 
     // DoRepeating
@@ -70,6 +109,27 @@ public abstract class EventMonoBehaviour : MonoBehaviour {
         AssertBlittable(t);
         return DoWaitForHelper(func(t));
     }
+    protected Task DoWaitFor<T, U>(Func<T, U, IEnumerator> func, T t, U u)
+            where T : struct
+            where U : struct {
+        AssertBlittable(t, u);
+        return DoWaitForHelper(func(t, u));
+    }
+    protected Task DoWaitFor<T, U, V>(Func<T, U, V, IEnumerator> func, T t, U u, V v)
+            where T : struct
+            where U : struct
+            where V : struct {
+        AssertBlittable(t, u, v);
+        return DoWaitForHelper(func(t, u, v));
+    }
+    protected Task DoWaitFor<T, U, V, W>(Func<T, U, V, W, IEnumerator> func, T t, U u, V v, W w)
+            where T : struct
+            where U : struct
+            where V : struct
+            where W : struct {
+        AssertBlittable(t, u, v, w);
+        return DoWaitForHelper(func(t, u, v, w));
+    }
 
     // DoWaitFor - User is responsible for triggering these TaskCompletionSources
 
@@ -83,6 +143,33 @@ public abstract class EventMonoBehaviour : MonoBehaviour {
         AssertBlittable(t);
         var tcs = new TaskCompletionSource<bool>();
         manager.events.Enqueue(func(tcs, t));
+        return tcs.Task;
+    }
+    protected Task DoWaitFor<T, U>(Func<TaskCompletionSource<bool>, T, U, IEnumerator> func, T t, U u)
+            where T : struct
+            where U : struct {
+        AssertBlittable(t, u);
+        var tcs = new TaskCompletionSource<bool>();
+        manager.events.Enqueue(func(tcs, t, u));
+        return tcs.Task;
+    }
+    protected Task DoWaitFor<T, U, V>(Func<TaskCompletionSource<bool>, T, U, V, IEnumerator> func, T t, U u, V v)
+            where T : struct
+            where U : struct
+            where V : struct {
+        AssertBlittable(t, u, v);
+        var tcs = new TaskCompletionSource<bool>();
+        manager.events.Enqueue(func(tcs, t, u, v));
+        return tcs.Task;
+    }
+    protected Task DoWaitFor<T, U, V, W>(Func<TaskCompletionSource<bool>, T, U, V, W, IEnumerator> func, T t, U u, V v, W w)
+            where T : struct
+            where U : struct
+            where V : struct
+            where W : struct {
+        AssertBlittable(t, u, v, w);
+        var tcs = new TaskCompletionSource<bool>();
+        manager.events.Enqueue(func(tcs, t, u, v, w));
         return tcs.Task;
     }
 
@@ -105,6 +192,27 @@ public abstract class EventMonoBehaviour : MonoBehaviour {
         AssertBlittable(t);
         return DoGetHelper<Z>(func(t));
     }
+    protected Task<Z> DoGet<T, U, Z>(Func<T, U, IEnumerator> func, T t, U u)
+            where T : struct
+            where U : struct {
+        AssertBlittable(t, u);
+        return DoGetHelper<Z>(func(t, u));
+    }
+    protected Task<Z> DoGet<T, U, V, Z>(Func<T, U, V, IEnumerator> func, T t, U u, V v)
+            where T : struct
+            where U : struct
+            where V : struct {
+        AssertBlittable(t, u, v);
+        return DoGetHelper<Z>(func(t, u, v));
+    }
+    protected Task<Z> DoGet<T, U, V, W, Z>(Func<T, U, V, W, IEnumerator> func, T t, U u, V v, W w)
+            where T : struct
+            where U : struct
+            where V : struct
+            where W : struct {
+        AssertBlittable(t, u, v, w);
+        return DoGetHelper<Z>(func(t, u, v, w));
+    }
 
     // DoGet - User is responsible for triggering these TaskCompletionSources
 
@@ -118,6 +226,33 @@ public abstract class EventMonoBehaviour : MonoBehaviour {
         AssertBlittable(t);
         var tcs = new TaskCompletionSource<Z>();
         manager.events.Enqueue(func(tcs, t));
+        return tcs.Task;
+    }
+    protected Task<Z> DoGet<T, U, Z>(Func<TaskCompletionSource<Z>, T, U, IEnumerator> func, T t, U u)
+            where T : struct
+            where U : struct {
+        AssertBlittable(t, u);
+        var tcs = new TaskCompletionSource<Z>();
+        manager.events.Enqueue(func(tcs, t, u));
+        return tcs.Task;
+    }
+    protected Task<Z> DoGet<T, U, V, Z>(Func<TaskCompletionSource<Z>, T, U, V, IEnumerator> func, T t, U u, V v)
+            where T : struct
+            where U : struct
+            where V : struct {
+        AssertBlittable(t, u, v);
+        var tcs = new TaskCompletionSource<Z>();
+        manager.events.Enqueue(func(tcs, t, u, v));
+        return tcs.Task;
+    }
+    protected Task<Z> DoGet<T, U, V, W, Z>(Func<TaskCompletionSource<Z>, T, U, V, W, IEnumerator> func, T t, U u, V v, W w)
+            where T : struct
+            where U : struct
+            where V : struct
+            where W : struct {
+        AssertBlittable(t, u, v, w);
+        var tcs = new TaskCompletionSource<Z>();
+        manager.events.Enqueue(func(tcs, t, u, v, w));
         return tcs.Task;
     }
 }
